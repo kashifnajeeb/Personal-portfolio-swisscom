52aa04530e0a4d37b39466077622a08668df5a3a
kashifnajeeb <aashinajeeb@gmail.com>
2023-05-11 16:48:42 +0200

    header issues fixed and converted to classes

--- js/header.js
+++ js/header.js
@@ -1,34 +1,81 @@
-// Get Scroll Position
-let lastScrollPosition = window.pageYOffset;
-
-// Selectors
-const header = document.querySelector(".section-header");
-const mobileNavbarCheckbox = document.getElementById("section-header-check");
-const headerLinks = document.querySelectorAll(".section-header-navbar-link");
-
-// Scroll event listener
-window.addEventListener("scroll", function () {
-  if (mobileNavbarCheckbox.checked) {
-    // Mobile navbar is open, do not apply scrolling logic
-    return;
+class Header {
+  constructor() {
+    // Selectors
+    this.header = document.querySelector(".section-header");
+    this.mobileNavbarCheckbox = document.getElementById("section-header-check");
+    this.headerLinks = document.querySelectorAll(".section-header-navbar-link");
+    // Checking header at Top
+    this.lastScrollPosition = 0;
+    this.isHeaderAtTop = true;
   }
 
-  const currentScrollPosition = window.pageYOffset;
+  init() {
+    this.adjustHeaderStyle(this.lastScrollPosition);
+    this.addListeners();
+  }
 
-  // Showing header only when scrolling up
-  header.style.top =
-    currentScrollPosition < lastScrollPosition ? "0" : "-10rem";
+  addListeners() {
+    // Scroll event listener
+    window.addEventListener("scroll", () => {
+      if (this.mobileNavbarCheckbox.checked) {
+        // Mobile navbar is open, do not apply scrolling logic
+        return;
+      }
 
-  lastScrollPosition = currentScrollPosition;
+      const currentScrollPosition = window.pageYOffset;
+      const isScrollingUp = currentScrollPosition < this.lastScrollPosition;
 
-  // Add shadow when scrolling down
-  header.style.boxShadow = currentScrollPosition > 0 ? "var(--shadow)" : "none";
-});
+      // Show or hide the header based on scroll direction
+      this.showHideHeader(isScrollingUp);
 
-// Click event listener for header links
-headerLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    // Uncheck the responsive header checkbox
-    mobileNavbarCheckbox.checked = false;
-  });
-});
+      // Adjust the header style based on scroll position
+      this.adjustHeaderStyle(currentScrollPosition);
+
+      this.lastScrollPosition = currentScrollPosition;
+    });
+
+    // Click event listener for header links
+    this.headerLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        // Uncheck the responsive header checkbox
+        this.mobileNavbarCheckbox.checked = false;
+      });
+    });
+  }
+
+  showHideHeader(isScrollingUp) {
+    this.header.style.top = isScrollingUp ? "0" : "-10rem";
+  }
+
+  adjustHeaderStyle(currentScrollPosition) {
+    if (currentScrollPosition > 5) {
+      // Header is not at the top and scrolling is more than 5 pixels
+      this.setHeaderStyle(
+        "8rem",
+        "var(--shadow)",
+        "saturate(180%) blur(20px)",
+        "rgba(237, 236, 255, 0.3)"
+      );
+      this.isHeaderAtTop = false;
+    } else {
+      // Header is at the top or scrolling is less than or equal to 5 pixels
+      if (!this.isHeaderAtTop) {
+        // Header was not at the top, reset styling
+        this.setHeaderStyle("10rem", "none", "none", "var(--bg)");
+        this.isHeaderAtTop = true;
+      }
+    }
+  }
+
+  setHeaderStyle(height, boxShadow, backdropFilter, backgroundColor) {
+    this.header.style.height = height;
+    this.header.style.boxShadow = boxShadow;
+    this.header.style.backdropFilter = backdropFilter;
+    this.header.style.webkitBackdropFilter = backdropFilter;
+    this.header.style.backgroundColor = backgroundColor;
+  }
+}
+
+// Initialize the header functionality
+const header = new Header();
+header.init();
