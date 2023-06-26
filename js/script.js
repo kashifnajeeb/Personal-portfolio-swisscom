7e5f42ae3356eeebfc64cac19184ad76832eaf49
kashifnajeeb <aashinajeeb@gmail.com>
2023-06-26 02:07:43 +0200

    added Typewriting effect in hero section logo using typewriter effect library

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@
 
 // Showing current Year in footer
 document.getElementById("current-year").textContent = new Date().getFullYear();
+
+// Typewriter Effect in Hero Section Logo
+var typewriter = new Typewriter(".section-hero-logo", {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString("KN")
+  .pauseFor(500)
+  .deleteChars(1)
+  .typeString("ashif Najeeb")
+  .pauseFor(500)
+  .start();
